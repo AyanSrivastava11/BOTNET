24cab4037825f8c9fa9bf457a48cf92c49002c37
AyanSrivastava11 <ayansrivastava322@gmail.com>
2022-12-02 18:23:22 +0530

    secondt commit

---
+++ arearect.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int l,b,h;
+
+    cout<<"lengt of rect = "<<endl;
+    cin>>l;
+
+    cout<<"width of rectangle = "<<endl;
+    cin>>b;
+
+    cout<<"height of rect = "<<endl;
+    cin>>h;
+
+    cout<<"area of rectangle = "<<l*b*h;
+    cout<<" sq cm";
+
+    return 0;
+
+}
